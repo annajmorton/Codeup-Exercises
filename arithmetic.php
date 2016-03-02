7f9d319c0e6945b0415c24b40fb4f4de21093ef2
Anna Morton <annajmorton@gmail.com>
2016-03-02 09:34:16 -0600

    added throw error message function and divZero
    These functions check the arguments passed to arithmatic functions to ensure the values can be operated upon

--- arithmetic.php
+++ arithmetic.php
@@ -1,36 +1,128 @@
 <?php
 
+function throwErrorMessage($a, $b) {
+
+	$turnItOff = false;
+
+	if (!is_numeric($a)) {
+		
+		echo "$a is not numeric, please pass only numbers to the function.";
+		$turnItOff = true;
+	
+	}
+
+	if (!is_numeric($b)) {
+		
+		echo "$b is not numeric, please pass only numbers to the function.";
+		$turnItOff = true;
+	
+	}
+
+	if ($turnItOff) {
+		
+		return true;
+
+	}
+
+
+	return false;
+
+
+}
+
+function divZero($b) {
+
+	if ($b == 0) {
+		echo "you entered $b, you cannot divide by zero";
+		return true;
+	}
+
+}
+
+
 function add($a, $b)
 {
+    
+	if (throwErrorMessage($a, $b)) {
+		
+		return;
+	} 
+
     return $a + $b;
 }
 
 function subtract($a, $b)
 {
+
+    
+	if (throwErrorMessage($a, $b)) {
+		
+		return;
+	} 
+		
     return $a - $b;
+	
 }
 
 function multiply($a, $b)
 {
+    
+	if (throwErrorMessage($a, $b)) {
+		
+		return;
+	} 
+
     return $a * $b;
+    	
 }
 
 function divide($a, $b)
 {
+    
+	if (throwErrorMessage($a, $b)) {
+		
+		return;
+	} 
+
+	if (divZero($b)) {
+		
+		return;
+	}
+
     return $a / $b;
+
 }
 function modulus($a ,$b){
 
+	if (throwErrorMessage($a, $b)) {
+		
+		return;
+	} 
+
+	if (divZero($b)) {
+
+		return;
+	}
+		
+	
 	return $a % $b;
+
+
 }
 
 echo 'function party!'.PHP_EOL;
 
-$good = 2;
-$bad = 5;
+
+fwrite(STDOUT, "Please give me two numbers and we can operate on them - yay!\n");
+fwrite(STDOUT, "enter number one\n");
+$good = trim(fgets(STDIN));
+
+
+fwrite(STDOUT, "enter number two\n");
+$bad = trim(fgets(STDIN));
+echo PHP_EOL . PHP_EOL;
 
 $ulgy = add($good, $bad);
-
 echo $ulgy . PHP_EOL;
 
 
@@ -38,7 +130,7 @@ echo subtract($good, $bad) . PHP_EOL;
 
 fwrite(STDOUT, multiply($good ,$bad) . PHP_EOL);
 
-echo $ulgy = divide($good, $bad);
+echo $ulgy = divide($good, $bad) . PHP_EOL;
 
 fwrite(STDOUT, modulus($good, $bad) . PHP_EOL);
 
