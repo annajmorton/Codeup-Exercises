fffaeb629c631a856a2e27ca8da0070671ed0cd9
Anna Morton <annajmorton@gmail.com>
2016-03-03 13:45:00 -0600

    added more steps to the excercise

--- list_with_commas.php
+++ list_with_commas.php
@@ -3,9 +3,25 @@
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 $physicistsArray = explode(', ', $physicistsString);
 
-array_push($physicistsArray, ("and " . array_pop($physicistsArray) . PHP_EOL));
 
-$famousFakePhysicists = implode(", ", $physicistsArray);
+function humanizedList($ArrayIN, $isAsort = false) {
 
+	if ($isAsort) {
+		
+		sort($ArrayIN);
+
+	}
+
+	array_push($ArrayIN, ("and " . array_pop($ArrayIN) . PHP_EOL));
+
+	$famousFakePhysicists = implode(", ", $ArrayIN);
+
+	return $famousFakePhysicists;
+	
+}
+
+
+// Humanize that list
+ $famousFakePhysicists = humanizedList($physicistsArray, true);
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
